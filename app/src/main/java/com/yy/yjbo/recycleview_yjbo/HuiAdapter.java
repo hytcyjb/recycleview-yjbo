724d64e444110439f6777b399a2d97ea54c1127d
yjbo-PC\yjbo <1457521527@qq.com>
2017-03-29 17:59:09 +0800

    添加分割线

---
+++ app/src/main/java/com/yy/yjbo/recycleview_yjbo/HuiAdapter.java
@@ -0,0 +1,184 @@
+package com.yy.yjbo.recycleview_yjbo;
+
+import android.content.Context;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @author yjbo
+ * @descr 汇的页面，动态显示内部的按钮
+ * @time 2016/9/9 14:16
+ */
+public class HuiAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    public static final int TYPE_HEADER = 0;
+    public static final int TYPE_NORMAL = 1;
+    public static final int TYPE_FOOTER = 2;
+    private List<HashMap<String, Object>> mlistHash;
+    private Context mContext;
+    private View mHeaderView;
+    private View mfooterView;
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+        RecyclerView.LayoutManager manager = recyclerView.getLayoutManager();
+        if (manager instanceof GridLayoutManager) {
+            final GridLayoutManager gridManager = ((GridLayoutManager) manager);
+            gridManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+                @Override
+                public int getSpanSize(int position) {
+                    if (getItemViewType(position) == TYPE_HEADER){
+                        return gridManager.getSpanCount();
+                    }else  if (getItemViewType(position) == TYPE_FOOTER){
+                        return gridManager.getSpanCount();
+                    }else {
+                        return 1;
+                    }
+//                    return getItemViewType(position) == TYPE_HEADER
+//                            ? gridManager.getSpanCount() : 1
+//                            || getItemViewType(position) == TYPE_FOOTER ?gridManager.getSpanCount(): 1;
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onViewAttachedToWindow(RecyclerView.ViewHolder holder) {
+        super.onViewAttachedToWindow(holder);
+        ViewGroup.LayoutParams lp = holder.itemView.getLayoutParams();
+        if (lp != null
+                && lp instanceof StaggeredGridLayoutManager.LayoutParams
+                && holder.getLayoutPosition() == 0) {
+            StaggeredGridLayoutManager.LayoutParams p = (StaggeredGridLayoutManager.LayoutParams) lp;
+            p.setFullSpan(true);
+        }
+    }
+
+    public void bindData(List<HashMap<String, Object>> listHash, Context context) {
+        this.mlistHash = listHash;
+        this.mContext = context;
+        notifyDataSetChanged();
+    }
+
+    public void setHeaderView(View headerView) {
+        mHeaderView = headerView;
+        notifyItemInserted(0);
+    }
+//    public void setSencodHeaderView(View headerView) {
+//        mHeaderView = headerView;
+//        notifyItemInserted(0);
+//    }
+
+    public void updateHeaderView(View headerView) {
+        mHeaderView = headerView;
+        notifyItemChanged(0);
+    }
+    /**
+     * 添加一个底部布局，为了留白
+     * @author yjbo  @time 2016/11/28 15:19
+     */
+    public void addfoot(View footerView) {
+        mfooterView = footerView;
+        notifyItemChanged(mlistHash.size());
+    }
+
+    public View getHeaderView() {
+        return mHeaderView;
+    }
+
+    private OnItemClickListener mListener;
+
+    public void setOnItemClickListener(OnItemClickListener li) {
+        mListener = li;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (mHeaderView == null) return TYPE_NORMAL;
+        if (mfooterView == null) return TYPE_NORMAL;
+        if (position == 0) return TYPE_HEADER;
+        if (position == mlistHash.size()+1) return TYPE_FOOTER;//position == mlistHash.size()+1;相当于加了头尾俩个了
+        return TYPE_NORMAL;
+    }
+
+    @Override
+    public huiHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (mHeaderView != null && viewType == TYPE_HEADER) return new huiHolder(mHeaderView);
+        if (mfooterView != null && viewType == TYPE_FOOTER) return new huiHolder(mfooterView);
+        View layout = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_hui_layout, parent, false);
+        return new huiHolder(layout);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if (getItemViewType(position) == TYPE_HEADER) return;
+        if (getItemViewType(position) == TYPE_FOOTER) return;
+        final int pos = getRealPosition(holder);
+        String imageInt = (String) mlistHash.get(pos).get("imageInt");
+        final String wordStr = (String) mlistHash.get(pos).get("wordStr");
+        final String compoUrl = (String) mlistHash.get(pos).get("compoUrl");
+        final String compoCode = (String) mlistHash.get(pos).get("compoCode");
+        if (mListener == null) return;
+        ((huiHolder) holder).itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    mListener.onItemClick(pos, compoUrl, compoCode, wordStr);
+            }
+        });
+    }
+
+    public int getRealPosition(RecyclerView.ViewHolder holder) {
+        int position = holder.getLayoutPosition();
+//        if(mHeaderView != null){
+//            position--;
+//        }
+//        if (mfooterView != null){
+//            position--;
+//        }
+        return position -1;
+//        return mHeaderView == null ? position : position - 1;
+    }
+
+    @Override
+    public int getItemCount() {
+        int totalCount = 0;
+        if(mHeaderView != null){
+            totalCount++;
+        }
+        if (mfooterView != null){
+            totalCount++;
+        }
+        return  totalCount+mlistHash.size();
+//        return mHeaderView == null ? mlistHash.size() : mlistHash.size() + 1;
+    }
+
+    public class huiHolder extends RecyclerView.ViewHolder {
+        ImageView navigItemIbt;
+        TextView itemTxt;
+        //        TextView buttom_white;
+        LinearLayout item_layout;
+        public huiHolder(View itemView) {
+            super(itemView);
+            if (itemView == mHeaderView) return;
+            if (itemView == mfooterView) return;
+            itemTxt = (TextView) itemView.findViewById(R.id.navig_item_txt);
+//            buttom_white = (TextView) itemView.findViewById(R.id.buttom_white);
+            item_layout = (LinearLayout) itemView.findViewById(R.id.item_layout);
+            navigItemIbt = (ImageView) itemView.findViewById(R.id.navig_item_ibt);
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(int position, String url, String compoCode, String wordStr);
+    }
+}
--- app/src/main/java/com/yy/yjbo/recycleview_yjbo/MainActivity.java
+++ app/src/main/java/com/yy/yjbo/recycleview_yjbo/MainActivity.java
@@ -2,13 +2,74 @@ package com.yy.yjbo.recycleview_yjbo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * recycleview的各种封装和使用
+ *
+ * @author yjbo
+ * @time 2017/3/29 16:59
+ * @mail 1457521527@qq.com
+ */
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView swipeTarget;
+    private MainActivity mContext;
+    private int showGideCount = 3;
+    private HuiAdapter huiAdapter;
+    private List<HashMap<String, Object>> listHash = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext = this;
+        initView();
+        getData();
+    }
 
+
+    private void initView() {
+        swipeTarget = (RecyclerView) findViewById(R.id.swipe_target);
+        GridLayoutManager layoutManager = new GridLayoutManager(mContext, showGideCount);//网格布局
+        swipeTarget.setLayoutManager(layoutManager);
+        swipeTarget.addItemDecoration(new DividerGridItemDecoration(mContext));
+
+        huiAdapter = new HuiAdapter();
+
+    }
+
+    private void getData() {
+        listHash.clear();
+
+        for (int i = 0; i < 8; i++) {
+            HashMap<String, Object> hashMap = new HashMap<>();
+            hashMap.put("imageInt", "");
+            hashMap.put("acid", "acid" + i);
+            hashMap.put("wordStr", "wordStr" + i);//真实名称
+            hashMap.put("compoCode", "compoCode" + i);//别名
+            hashMap.put("compoUrl", "compoUrl" + i);
+            hashMap.put("biaozhi", "biaozhi" + i);
+            listHash.add(hashMap);
+        }
+        huiAdapter.bindData(listHash, mContext);
+        setHeader(swipeTarget);
+        swipeTarget.setAdapter(huiAdapter);
+    }
+    private void setHeader(RecyclerView recyclerView) {
+        View header = LayoutInflater.from(mContext).inflate(R.layout.fragment_navigation_footer, recyclerView, false);
+
+        View footer = LayoutInflater.from(mContext).inflate(R.layout.fragment_navigation_footer, recyclerView, false);
+        huiAdapter.setHeaderView(header);
+        huiAdapter.addfoot(footer);
     }
 }
